1fdd97bf4d597e895d30f33a7971bd4cecdfc30f
Andrej Bernhard <andrej.bernhard@suncar-ag.com>
2025-02-28 11:11:13 +0100

    plotting predictions

---
+++ results/plots/plots.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pls
+
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src"))
+print(src_path)
+sys.path.append(src_path)
+
+# Now import data_loader
+from data_loader import CrossBorderData
+
+model_name = "linreg"
+pred_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../results/predictions', f"{model_name}.csv")
+
+test_dataset = CrossBorderData(train=False) 
+actual_df = pd.DataFrame({
+    "timestamp": test_dataset.index,
+    "actual_capacity": test_dataset.y.numpy().flatten()
+})
+
