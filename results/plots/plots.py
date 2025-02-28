aa33c0aa705ba1d642d322c664326e5c8c9135c2
beandrej <beandrej@ethz.ch>
2025-02-28 16:22:32 +0100

    first comparison nn vs linreg

--- results/plots/plots.py
+++ results/plots/plots.py
@@ -2,23 +2,43 @@ import os
 import sys
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as pls
+import matplotlib.pyplot as plt
+
 
 src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src"))
-print(src_path)
 sys.path.append(src_path)
 
-# Now import data_loader
 from data_loader import CrossBorderData
+import config
 
-model_name = "linreg"
-pred_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../results/predictions', f"{model_name}.csv")
+save_plot = True
+model_name = config.MODEL_NAME
 
-test_dataset = CrossBorderData(train=False) 
-actual_df = pd.DataFrame({
-    "timestamp": test_dataset.index,
-    "actual_capacity": test_dataset.y.numpy().flatten()
+pred_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../predictions', f"{model_name}.csv")
+predictions_df = pd.read_csv(pred_path)
+predictions_df = pd.DataFrame({
+    "timestamp": pd.to_datetime(predictions_df["timestamp"]),
+    "predicted_capacity": predictions_df["predicted_capacity"]
 })
 
-# TODO maybe change CrossBorderData to pd.Dataframe default -> convert to pytorch tensor in train.py file
-# TODO plot some first results
+test_dataset = CrossBorderData(train=False).data
+actual_df = pd.DataFrame({
+    "timestamp": pd.to_datetime(test_dataset.index),
+    "actual_capacity": test_dataset["cross_border_capacity"].to_numpy().flatten()
+})
+
+comparison_df = actual_df.merge(predictions_df, on="timestamp")
+save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'figures', f"{model_name}.png")
+
+plt.figure(figsize=(12, 6))
+plt.plot(comparison_df["timestamp"], comparison_df["actual_capacity"], label="Actual Capacity", color="blue")
+plt.plot(comparison_df["timestamp"], comparison_df["predicted_capacity"], label="Predicted Capacity", color="red", linestyle="dashed")
+plt.xlabel("Timestamp")
+plt.ylabel("Cross-Border Capacity")
+plt.title("Predicted vs. Actual Cross-Border Transmission Capacity")
+plt.legend()
+plt.grid()
+if save_plot:
+    plt.savefig(save_path, dpi=300, bbox_inches='tight')
+    print("Figure saved at:", f"{save_path}")
+plt.show()
